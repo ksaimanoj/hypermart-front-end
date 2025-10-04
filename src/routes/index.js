149cc13c995b36d147b2a83e4feae86016214e09
Sai Manoj Kadiyala <saimanoj.kadiyala@gmail.com>
2025-10-04 06:09:48 +0530

    Add new route /db-test to test DB connection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false } // Neon requires SSL
+});
 
 router.get('/', (req, res) => {
     res.send('Welcome to Hyper Mart!');
+});
+
+router.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
